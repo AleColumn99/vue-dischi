2938d7f81c78d22bb617555ed4773e190b52312c
AleColumn99 <alecolumn.99@gmail.com>
2021-05-24 20:59:07 +0200

    funziona?

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div id="app">
+
+    <div v-if="!loading" class="container text-center mt-5">
+  
+      <div class="wrap d-flex justify-content-center align-items-center" >
+
+        <Card
+          v-for="card in cards"
+          :key="card.id"
+          :card="card"
+        />
+        
+      </div>
+
+    </div>
+
+    <Loading title="Albums" v-else />
+
+  </div>
+
+</template>
+
+<script>
+
+import axios from 'axios';
+import Card from '@/components/Card';
+import Loading from '@/components/Loading';
+
+export default {
+  name: 'App',
+  components: {
+    Card,
+    Loading
+  },
+  data() {
+    return {
+      axios,
+      cards: [],
+      loading: true
+    }
+  },
+  created() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then( res => {
+      console.log(res.data);
+      this.loading = false;
+    })
+    .catch( err => {
+      console.log(err);
+    })
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+@import '@/assets/style/general';
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="col-sm-6 col-md-4 col-lg-3 mb-5">
+    <img :src="card.poster" alt="card.name">
+    <h3>{{ card.title }}</h3>
+    <h5>{{ card.author }}</h5>
+    <h5>{{ card.year }}</h5>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    card: Object
+  }
+}
+</script>
+
+<style>
+
+
+
+</style>
